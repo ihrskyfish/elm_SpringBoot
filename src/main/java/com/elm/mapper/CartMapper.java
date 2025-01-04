db972864af88093c8a84af9e93ea6651b6cdd737
ihrskyfish <ihr17867950748@126.com>
2025-01-04 15:07:48 +0800

    remane  sql in dao

--- src/main/java/com/elm/mapper/CartMapper.java
+++ src/main/java/com/elm/mapper/CartMapper.java
@@ -12,15 +12,15 @@ import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface CartMapper extends BaseMapper<Cart> {
-    public List<Cart> listCart(String userId, Integer businessId);
+    public List<Cart> listCart(String userId, Integer merchantId);
 
-    @Insert("insert into cart(foodId, businessId, userId, quantity, isDelete) values(#{foodId},#{businessId},#{userId},1,0)")
-    public int saveCart(Integer businessId, String userId, Integer foodId) throws SQLException;
+    @Insert("insert into cart(foodId, merchantId, userId, quantity, isDelete) values(#{foodId},#{merchantId},#{userId},1,0)")
+    public int saveCart(Integer merchantId, String userId, Integer foodId) throws SQLException;
 
-    @Select("select * from cart where foodId=#{foodId} and businessId=#{businessId} and userId=#{userId}")
-    public Cart getCartById(Integer foodId, Integer businessId, String userId) throws SQLException;
-    @Update("update cart set quantity=#{quantity} where foodId=#{foodId} and businessId=#{businessId} and userId=#{userId}")
-    public int updateCart(Integer businessId, Integer foodId, String userId, Integer quantity) throws SQLException;
+    @Select("select * from cart where foodId=#{foodId} and merchantId=#{merchantId} and userId=#{userId}")
+    public Cart getCartById(Integer foodId, Integer merchantId, String userId) throws SQLException;
+    @Update("update cart set quantity=#{quantity} where foodId=#{foodId} and merchantId=#{merchantId} and userId=#{userId}")
+    public int updateCart(Integer merchantId, Integer foodId, String userId, Integer quantity) throws SQLException;
 
-    public int removeCart(String userId, Integer businessId, Integer foodId) throws SQLException;
+    public int removeCart(String userId, Integer merchantId, Integer foodId) throws SQLException;
 }
